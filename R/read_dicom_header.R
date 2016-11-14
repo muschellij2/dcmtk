ab822ea6cbf800ddd428c9d3e7a0a68161f8e9e5
muschellij2 <muschellij2@gmail.com>
2016-11-14 13:48:40 -0500

    Added replace_names.

--- R/read_dicom_header.R
+++ R/read_dicom_header.R
@@ -2,6 +2,8 @@
 #' @description Reads in the DICOM header from a file
 #'
 #' @param file Name of DICOM file
+#' @param replace_names logical indicating if unknown tag names should be
+#' inferred from \code{dicom_tags}
 #'
 #' @return Character vector of header information
 #' @export
@@ -12,7 +14,7 @@ read_dicom_header = function(file, replace_names = FALSE) {
   hdr = parse_hdr(hdr)
   if (replace_names) {
     hdr$ind = seq(nrow(hdr))
-    tag =  dcmtk::dicom_tags[, c("tag", "name")]
+    tag =  dcmtk::dicom_tags[, c("tag", "keyword")]
     colnames(tag)[2] = "dname"
     hdr = merge(hdr, tag, all.x = TRUE, sort = FALSE)
     ind = hdr$name == "UnknownTagAndData" & !is.na(hdr$dname)
